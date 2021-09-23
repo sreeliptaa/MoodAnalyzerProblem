a44839564b2cd89132af00ef7ffed4b69822b536
sreeliptaa <sreelipta65@gmail.com>
2021-09-24 01:45:12 +0530

    Added: welcome message

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : Implementing Mood Analyser Program
+ * @author Sreelipta
+ * @since 2021-09-24
+ */
+
+public class MoodAnalyzer {
+    public static void main(String[] args) {
+        System.out.println("Welcom to Mood Analyzer Program");
+    }
+}
